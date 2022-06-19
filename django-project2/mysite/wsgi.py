b0ad9ce5a9e329f1ebc831aa4cdeb00e0d6d9a1d
olamide1738 <102264645+olamide1738@users.noreply.github.com>
2022-06-19 12:46:34 +0200

    Add files via upload

---
+++ django-project2/mysite/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for icey project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+import sys
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+
+path = '/home/path/to/project'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
